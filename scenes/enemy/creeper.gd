c19cb39477db5d4a6f7421aeee93b584360ceed3
Esosek <67476790+Esosek@users.noreply.github.com>
2024-08-25 07:43:46 +0200

    Added Enemy base and Crepeer class

---
+++ scenes/enemy/creeper.gd
@@ -0,0 +1,21 @@
+class_name Creeper
+extends Enemy
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print(health)
+
+
+# Godot volá každý frame. Používá se pro všechno kromě fyzikálního pohybu
+# parametr delta je čas mezi framy a slouží pro konzistentní update.
+func _process(_delta):
+	#follow_player("Pronásleduji hráče")
+	pass
+	
+# Godot volá každý frame. Používá se pro fyzikální pohyb
+func _physics_process(_delta):
+	pass
+
+func follow_player(zprava: String):
+	print(zprava)
---
+++ scenes/enemy/enemy.gd
@@ -0,0 +1,6 @@
+class_name Enemy
+extends Node
+
+@export var health: int
+@export var damage: int
+@export var speed: float
